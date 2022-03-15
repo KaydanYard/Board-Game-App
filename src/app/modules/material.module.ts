452d1a6482c015e9de106397645473c8f263b42b
KaydanYard <86071974+KaydanYard@users.noreply.github.com>
2022-03-15 09:59:45 -0600

    working on part 08: save user selections

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -7,6 +7,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card'
 
 @NgModule({
   declarations: [],
@@ -18,7 +19,8 @@ import { MatInputModule } from '@angular/material/input';
     MatIconModule,
     MatInputModule,
     MatButtonModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatCardModule
   ],
   exports: [
     MatToolbarModule,
@@ -27,7 +29,8 @@ import { MatInputModule } from '@angular/material/input';
     MatIconModule,
     MatInputModule,
     MatButtonModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatCardModule
   ]
 })
 export class MaterialModule { }
